d7f68c2da7de89e9b84a12c64067d6e966ceacd9
Maftuna Komilova <kamilovamaftuna2025@gmail.com>
2025-12-02 12:06:04 +0000

    done

--- index.js
+++ index.js
@@ -5,15 +5,3 @@ config()
 
 const TOKEN = process.env.BOT_TOKEN
 const bot = new TelegramBot(TOKEN , {polling: true})
-
-bot.on("message" , (msg) => {
-    const text = msg.text
-    const firstName = msg.chat.first_name
-    const chatId = msg.chat.id
-    //Start 
-    if(text == "/start") {
-        console.log("Start...");
-        console.log(msg);
-        
-    }
-})
